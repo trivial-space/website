cc9d98f0b9b24fe1c9c4febc7e0cce8685579d51
Thomas Gorny <thomas.gorny@pm.me>
2023-08-15 22:41:58 +0200

    blur

--- components/Work.tsx
+++ components/Work.tsx
@@ -7,8 +7,8 @@ interface Props {
 
 export default function Work({ img, title }: Props) {
 	return (
-		<div className="w-[600px] h-[600px] max-h-[60vh] max-w-[60vw] mx-12 md:mx-24 shrink-0 flex items-center">
-			<div className="object-contain w-full h-auto my-auto border-white rounded border border-4 shadow-xl shadow-slate-400">
+		<div className="w-[600px] h-[600px] max-h-[60vh] max-w-[60vw] mx-12 md:mx-24 shrink-0 flex items-center blur">
+			<div className="object-contain w-full h-auto my-auto border-white border-4 shadow-xl shadow-slate-400">
 				<Image alt={title} {...img} className="" />
 			</div>
 		</div>
